2271dd73dee5c802904dc60f27a23e6791f59508
Jason Jones <jasonwjones@gmail.com>
2022-02-17 16:41:48 -0800

    Additional REST client example

--- Java/Modules/essbase-rest-client/gen.sh
+++ Java/Modules/essbase-rest-client/gen.sh
@@ -11,3 +11,19 @@ java -jar openapi-generator-cli-5.1.1.jar generate \
 	--output target
 
 cp -R target/src/main/java/* ./src/main/java
+
+# Alternate invocation example:
+#java -jar openapi-generator-cli-5.2.0.jar generate \
+#-i http://localhost:8090/v3/api-docs \
+#-g java \
+#-p dateLibrary=java8 \
+#--library resttemplate  \
+#--api-package com.appliedolap.dodeca.cloud.api \
+#--package-name com.appliedolap.dodeca.cloud \
+#--group-id com.appliedolap.dodeca.cloud \
+#--model-package com.appliedolap.dodeca.cloud.model \
+#--artifact-id dodeca-cloud-client2 \
+#--artifact-version 1.0.0 \
+#--strict-spec \
+#--verbose
+#-additional-properties=licenseUrl=foo,java8=true,dateLibrary=joda \
